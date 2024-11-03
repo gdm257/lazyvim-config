7df2188ce492f3daae60e4fc00d88ac37c7c0eda
gdm257 <257@gdm.anonaddy.com>
2024-11-04 01:17:40 +0900

    💄 feat(ui): set fonts
    Fonts must have existed in system, otherwise errors will be reported

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+--- Neovide ---
+--
+-- see more in https://neovide.dev/configuration.html
+-- if vim.g.neovide then
+--
+-- end
+
+-- GUI fonts
+opt.guifont = {
+  "JetBrainsMono Nerd Font",
+  "LXGW WenKai Screen R",
+}
