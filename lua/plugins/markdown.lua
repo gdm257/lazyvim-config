9123c6cd7744854f47e5498bca277928670cf6dc
gdm257 <gdm@disroot.org>
2026-03-02 05:05:43 +0900

    fix(plugins): disable linting for markdown
    MDxxx is noisy.

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  -- Disable linting for markdown
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+}
